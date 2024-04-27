05422b5fcf76f4f503fc2137b95a084901e16155
unknown <lukmanolayiwola125@gmail.com>
2024-04-27 04:49:02 +0100

    set up cypress project

---
+++ cypress/e2e/AcessOne.spec.cy.js
@@ -0,0 +1,18 @@
+
+
+describe('Testing eCommerce Website', () =>{
+  beforeEach(()=>{
+    cy.visit('/')
+  })
+  
+    it('This is a test case 1', () =>{
+      cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+      cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+      cy.get('.oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button').click()
+    });
+
+    //it('This is a test case 2', () =>{
+    //});
+
+  
+  })
